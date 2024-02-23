823a1e1ef69f8b93ddd93ac114a67fb6298c2b6f
Paula Ramos-Silva <paula.rsilva@naturalis.nl>
2024-02-23 15:22:53 +0100

    format PEP8 update
    Update format and input and output file paths

--- get_presence_absence_from_blast.py
+++ get_presence_absence_from_blast.py
@@ -1,25 +1,30 @@
 __author__ = 'Paula Ramos-Silva'
+__dateCreated__ = "22-03-2016"
+__dateModified__ = "23-02-2024"
 
-# Reads txt and/or csv files from blast output and creates file with presence/absence of hits specified by the user
-# per sample.
-#
-# ======================================================================
-#
-# Tested with:
-#  Python 3.9.0
-#  using dataset from Ramos-Silva P, Wall-Palmer D, Marlétaz F, Marin F, Peijnenburg KTCA.
-#  Evolution and biomineralization of pteropod shells. J Struct Biol 2021; 213: 107779.
-#  https://doi.org/10.1016/j.jsb.2021.107779
-#
-# Created 22-03-2016
-# Updated 08-02-2024
+"""
+Reads txt and/or csv files in the input/ folder resulting from blast tabular output and prints a list with number of 
+hits per subject id. 
+
+usage: python get_presence_absence_from_blast.py -h
+
+Reference:
+Paula Ramos-Silva, Deborah Wall-Palmer, Ferdinand Marlétaz, Frédéric Marin, Katja T.C.A. Peijnenburg,
+Evolution and biomineralization of pteropod shells, Journal of Structural Biology, Volume 213, Issue 4,
+2021, 107779, https://doi.org/10.1016/j.jsb.2021.107779.
+
+Tested with:
+Python 3.9.0
+
+"""
 
 import argparse
 import os
 import sys
+from pathlib import Path
+
 
 def main():
-
     parser = argparse.ArgumentParser(description="Reads txt and csv files from blast output tabular format and prints "
                                                  "list of presence/absence of sequence hits specified by the user")
     parser.add_argument('--seq-hits', dest='seq_hits', help='text file with accession numbers and sequence names '
@@ -29,61 +34,68 @@ def main():
     args = parser.parse_args()
 
     # list of output files from blast
-    csv_files = filter(lambda x: x.endswith(args.file_suffix), os.listdir('.'))
+    csv_files = filter(lambda x: x.endswith(args.file_suffix), os.listdir('input'))
+    print(csv_files)
 
     # output file
-    myfile = open ('profile.txt', "a")
+    output_folder = Path("output/")
+    output_file = output_folder/ "profile.txt"
+    profile = open(output_file, "a")
 
     for csv in csv_files:
         # creates dictionary and prints subject_ids listed in seq_hits and corresponding query_ids that have a
         # match in the blast report
-        blastmatch_dic = create_dictionary_fromcommasepfile(csv)
-        print (csv)
-        for k, v in blastmatch_dic.items() :
-            print ("{}\t{}".format(k, v))
+        blast_match_dic = create_dictionary_from_csv(csv)
+        print(csv)
+        for k, v in blast_match_dic.items():
+            print("{}\t{}".format(k, v))
         lnoh = []
 
         # counts number of subject_ids (items) in dictionary values (lists) per sequence hit
         # and writes results to file profile.txt
-        with open (args.seq_hits, 'r') as seqhs:
-            for l in seqhs:
-                myl = l.strip()
-                acc = myl.split(',')[0]
-                if acc in blastmatch_dic.keys():
-                    nrofhits = len(blastmatch_dic[acc])
+        with open(args.seq_hits, 'r') as seqhs:
+            for hit in seqhs:
+                myhit = hit.strip()
+                acc = myhit.split(',')[0]
+                if acc in blast_match_dic.keys():
+                    nrofhits = len(blast_match_dic[acc])
                 else:
                     nrofhits = 0
 
                 lnoh.append(nrofhits)
 
-        myfile.write("{}\t{}\n".format(csv, lnoh))
+        profile.write("{}\t{}\n".format(csv, lnoh))
 
-    myfile.close()
+    profile.close()
 
 
-# This function creates a dictionary where keys are subject id(s) and values are the list of query_id(s) having
+"""
+This function creates a dictionary where keys are subject id(s) and values are the list of query_id(s) having
 # subject_id as hit in the blast report
+"""
 
-def create_dictionary_fromcommasepfile (myfile):
 
-    with open (myfile, 'r') as intable:
+def create_dictionary_from_csv(myfile):
+    input_folder = Path("input/")
+    file_to_parse = input_folder / myfile
+    with open(file_to_parse, 'r') as intable:
         next(intable)
         dic = {}
         for line in intable:
             li = line.rstrip()
             cols = li.split(',')
-            query_id = cols[0].split('-')[0] # may change depending on how query ID is named
+            query_id = cols[0].split('-')[0]  # may change depending on how query ID is named
             subject_id = cols[3]
-            dic.setdefault(subject_id,[]).append(query_id)
+            dic.setdefault(subject_id, []).append(query_id)
 
     return dic
 
 
 #########################
 
-if __name__=="__main__":
+if __name__ == "__main__":
     usage = "python get_presence_absence_from_blast.py -h"
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 5:
         print("Incorrect arguments.\nFor usage try: ", usage)
         sys.exit()
     main()
